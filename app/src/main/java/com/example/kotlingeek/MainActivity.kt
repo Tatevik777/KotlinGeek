fdb57318d7f73f48e963f85005203fc822e6d352
Tatevik777 <67191920+Tatevik777@users.noreply.github.com>
2023-03-05 17:26:34 +0300

    дз1

--- app/src/main/java/com/example/kotlingeek/MainActivity.kt
+++ app/src/main/java/com/example/kotlingeek/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.kotlingeek
-
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // get reference to button
+        val btn_press_me = findViewById(R.id.press_me_btn)as Button
+
+        val btn_press_me2 = findViewById(R.id.press_me_btn2)as Button
+
+// set on-click listener
+        btn_press_me.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Welcome to Kotlin", Toast.LENGTH_SHORT).show()
+
+
+
+            val student1= Student("Тата","пишет код")
+
+            btn_press_me2.setOnClickListener {
+                Toast.makeText(this@MainActivity, student1.toString(), Toast.LENGTH_SHORT).show()
+
+
+
+
+            }
+         }
     }
 }
---
+++ app/src/main/java/com/example/kotlingeek/Student.kt
@@ -0,0 +1,3 @@
+package com.example.kotlingeek
+
+data class Student(val name: String, var action: String)
